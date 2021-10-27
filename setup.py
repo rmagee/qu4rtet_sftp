1d7c45f2313dc5cb8f8a3b7132e05fe808f09ac8
Serial Lab <slab@serial-lab.com>
2021-10-27 14:38:49 -0600

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/serial-lab/quartet_sftp',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=False,
 )
